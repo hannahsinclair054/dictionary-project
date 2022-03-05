ed21107187233293c6fc95e25615a4cc14343cae
hannahsinclair054 <88463868+hannahsinclair054@users.noreply.github.com>
2022-03-05 11:41:15 +0000

    Updated synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,6 +4,7 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul className="Synonyms">
+        Similar:{" "}
         {props.synonyms.map(function (synonyms, index) {
           return <li key={index}>{synonyms}</li>;
         })}
